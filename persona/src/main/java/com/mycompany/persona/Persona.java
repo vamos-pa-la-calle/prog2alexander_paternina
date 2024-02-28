70e46140d3e97d311e64145bec26d30452919f75
vamos-pa-la-calle <160240350+vamos-pa-la-calle@users.noreply.github.com>
2024-02-28 09:04:52 -0500

    Add files via upload

---
+++ persona/src/main/java/com/mycompany/persona/Persona.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.persona;
+
+
+public class Persona {
+private String nombre;
+private String apellido;
+private String correo;
+private int cedula;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public int getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+  
+    
+}
---
+++ persona/src/main/java/com/mycompany/persona/personas.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.persona;
+import static javax.swing.text.html.HTML.Tag.P;
+public class personas {
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        p.setNombre(" alexander bovea");
+        System.out.print(p.getNombre());
+           
+    }
+    
+}
