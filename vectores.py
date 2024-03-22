5d8d042e59ac7620fd44948a0b8fae18e3383198
vamos-pa-la-calle <160240350+vamos-pa-la-calle@users.noreply.github.com>
2024-03-22 08:45:28 -0500

    tarea de vectores

---
+++ vectores.py
@@ -0,0 +1,46 @@
+a = []
+
+b = ['tiktok','netflix','unitecnar','optiplex','dell']
+
+length = len(a)
+print(length)
+
+length = len(b)
+print(length)
+
+IN = True,False
+print(b[0] + ',' + b[2]+ ','+b[4])
+
+datos_personales = ['alexander', '18', '1.80', 'soltero', 'emanuel']
+datos_personales.append('moreno')
+print(datos_personales)
+
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle','Amazon']
+it_companies.insert(0,'kawai')
+print(it_companies)
+
+does_exist = 'kawai' in it_companies
+print(does_exist)
+
+it_companies.sort()
+print(it_companies)
+
+it_companies.reverse()
+print(it_companies)
+
+it_companies.pop (2)
+print(it_companies)
+
+del it_companies[4]
+print(it_companies)
+
+it_companies.clear()
+print (it_companies)
+
+
+
+
+
+
+
+
