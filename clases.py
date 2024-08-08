2027f7a6b0ce42aaab367fdb84d9b7a313b437aa
vamos-pa-la-calle <160240350+vamos-pa-la-calle@users.noreply.github.com>
2024-08-08 11:54:10 -0500

    animal

---
+++ clases.py
@@ -0,0 +1,13 @@
+class clases:
+    def __init__(self,nombre,pesos):
+        self.nombre = nombre
+        self. peso = peso
+
+
+    def printname(self):
+        print(self.nombre,self.peso)
+
+
+x = clases("alexander", "71")
+x.printname()
+
